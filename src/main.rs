89dfb6edc8f1c30a8b211c2a1581fe8221e04a8b
tcfollett <follettsfive.toby@icloud.com>
2026-06-17 11:48:39 -0700

    made a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use glfw::{Action, Context, Key, fail_on_errors};
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: u32 = 800;
+    const HEIGHT: u32 = 600;
+
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(WIDTH, HEIGHT, "window", glfw::WindowMode::Windowed)
+        .unwrap();
+
+    window.set_all_polling(true);
+
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true);
+                }
+
+                _ => {}
+            }
+        }
+        window.swap_buffers();
+    }
 }
